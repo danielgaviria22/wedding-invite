fbdff1538bb34eeb7941baf4d7ae723c28807dac
Daniel Gaviria <danielgaviria61@gmail.com>
2025-03-27 15:12:14 -0500

    Add metadata and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,20 @@ const taviraj = Taviraj({
 export const metadata: Metadata = {
   title: "Te invitamos a nuestro matrimonio",
   description: "Nos complace invitarte a nuestra ceremonia",
+  openGraph: {
+    title: "Te invitamos a nuestro matrimonio",
+    description: "Nos complace invitarte a nuestra ceremonia",
+    url: "",
+    images: [
+      {
+        url: `./images/thumbnail.png`,
+        width: "1032",
+        height: "498",
+        alt: "Daniel Gaviria y Sebastian Casas",
+      },
+    ],
+    siteName: "Daniel Gaviria y Sebastian Casas",
+  },
 };
 
 export default function RootLayout({
