0032a4c7d796d5dae26d267e398bd57f70efd1b6
Daniel Gaviria <danielgaviria61@gmail.com>
2025-03-27 11:17:11 -0500

    Add Not found page styles

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,33 @@
+import Image from "next/image";
+
 function NotFoundPage() {
-  return <h1>Invitacion no valida. Contactanos para solucionar el problema</h1>;
+  return (
+    <section className="w-full h-lvh flex justify-center items-center bg-blue-darkest">
+      <div className="flex flex-col gap-12 items-center">
+        <Image
+          src="./images/blueNames.svg"
+          alt="Daniel Gaviria y Sebastian Casas"
+          width={256}
+          height={96}
+        />
+        <div className="relative flex flex-col items-center w-80">
+          <div className="absolute w-full h-[2px] bottom-5 bg-blue-light"></div>
+          <div className="absolute w-full h-px bottom-6 bg-blue-light"></div>
+          <Image
+            className="z-10"
+            src="./images/bigRedRose.svg"
+            alt="rose"
+            width={113}
+            height={93}
+          />
+        </div>
+        <div className="text-center text-blue-light">
+          <h2 className="font-medium text-2xl">¡Invitación no valida!</h2>
+          <p>Contáctanos para solucionar el problema.</p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default NotFoundPage;
